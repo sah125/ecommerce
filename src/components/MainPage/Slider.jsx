ded12afd0efcaa1fe2fce8a65000727b1bebbb1b
sah125 <sah.sakhile@gmail.com>
2024-02-02 09:54:17 +0200

    SLick curousel

--- src/components/MainPage/Slider.jsx
+++ src/components/MainPage/Slider.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import SlideCard from "./SlideCard";
 
 const Slider = () => {
   return (
     <>
-      <section className="homeSlide contentWidth"></section>
+      <section className="homeSlide contentWidth">
+        <div className="container">
+          <SlideCard />
+        </div>
+      </section>
     </>
   );
 };
